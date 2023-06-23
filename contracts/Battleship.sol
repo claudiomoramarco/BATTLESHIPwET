f3c1b02adc8fdb95b85c7a8b3e5fa03e754f9e08
claudiomoramarco <moramarco49@gmail.com>
2023-06-23 20:03:19 +0200

    fix sintassi sol

--- contracts/Battleship.sol
+++ contracts/Battleship.sol
@@ -1,49 +1,61 @@
-pragma solidity ^0.5.0; 
+pragma solidity ^0.5.0;
 
-contract Battleship{
+contract Battleship {
     address public giocatore1;
     address public giocatore2;
-    //mapping che associa un valore booleano a ciascun indirizzo Ethereum
-    mapping(addmod => bool) public navePiazzata;
+    mapping(address => bool) public navePiazzata;
     mapping(address => mapping(uint8 => mapping(uint8 => bool))) public griglia;
 
+    modifier soloGiocatori() {
+        require(msg.sender == giocatore1 || msg.sender == giocatore2, "Solo i giocatori possono eseguire questa azione");
+        _;
+    }
 
     constructor() public {
         giocatore1 = msg.sender;
     }
-}
-//modifier  per controllare se l'indirizzo dell'entita chiamante msg sender corrisponde ad uno dei due giocatori
-    modifier soloGiocatori() {
-        require(msg.sender == giocatore1 || msg.sender == giocatore2 "Solo i Giocatori possono eseguire questa azione"):
-        _; // Punto in cui il corpo della funzione verrà eseguito
+
+    function partecipaGioco() public {
+        require(giocatore2 == address(0), "Il gioco è al completo");
+        giocatore2 = msg.sender;
     }
 
-    //giocatore1 viene impostato nel costruttore senza la necessità di chiamare una funzione separata per impostare il primo giocatore.
-    function partecipaGioco () public {
-        require(giocatore2 == address(0),"il gioco è al completo");
-        giocatore2 = msg.sender;
-
-        } 
-     function piazzaNavi(uint8 x, uint8 y) public soloGiocatori {
+    function piazzaNavi(uint8 x, uint8 y) public soloGiocatori {
         require(!navePiazzata[msg.sender], "Hai già piazzato le navi");
         require(x < 10 && y < 10, "Coordinate non valide");
         require(!griglia[msg.sender][x][y], "Posizione già occupata");
-        
+
         griglia[msg.sender][x][y] = true;
     }
-        
-        //uint va bene per un 8x8 8bit da 0 a 7
-    function attaccoPosizione(uint8 x,uint8 y) public soloGiocatori {
+
+    function attaccoPosizione(uint8 x, uint8 y) public soloGiocatori {
         require(navePiazzata[giocatore1] && navePiazzata[giocatore2], "Attendi il piazzamento delle navi da entrambi i giocatori");
         require(x < 10 && y < 10, "Coordinate non valide");
         require(!griglia[msg.sender][x][y], "Hai già attaccato questa posizione");
-        
+
+        // Utilizza le coordinate (x, y) per determinare se l'attacco colpisce una nave o meno
     }
 
-    function impostaNaviPosizionate() public soloGiocatori  {
-        require(!navePiazzata[msg.sender],"hai segnalato che hai piazzato una nave");
-        require(giocatore1 != address(0) && giocatore2 != address(0),"attendi che entrambi i player si uniscano");
+    function impostaNaviPosizionate() public soloGiocatori {
+        require(!navePiazzata[msg.sender], "Hai già segnalato che hai piazzato le navi");
+        require(giocatore1 != address(0) && giocatore2 != address(0), "Attendi che entrambi i giocatori si uniscano");
 
         navePiazzata[msg.sender] = true;
-        
+
+        bool colpito = false;
+        address avversario = (msg.sender == giocatore1) ? giocatore2 : giocatore1;
+
+        uint8 x = 0; // Inserisci qui la coordinata x dell'attacco
+        uint8 y = 0; // Inserisci qui la coordinata y dell'attacco
+
+        if (griglia[avversario][x][y]) {
+            // L'attacco ha colpito una nave
+            colpito = true;
+            // Aggiungi qui la logica per gestire l'effetto dell'attacco sulla nave colpita
+        }
+
+        emit RisultatoAttacco(msg.sender, x, y, colpito);
     }
+
+    event RisultatoAttacco(address indexed player, uint8 x, uint8 y, bool colpito);
+}
